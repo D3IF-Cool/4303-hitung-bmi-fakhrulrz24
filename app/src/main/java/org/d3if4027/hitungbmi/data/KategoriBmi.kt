358ebeeacf58518932112cb9c2fef05fb7fc9ca6
Fakhrul_Razy <lilfawlz@student.telkomuniversity.ac.id>
2021-04-26 16:30:37 +0700

    Membuat navigasi ke app lain

---
+++ app/src/main/java/org/d3if4027/hitungbmi/data/KategoriBmi.kt
@@ -0,0 +1,5 @@
+package org.d3if4027.hitungbmi.data
+
+enum class KategoriBmi {
+    KURUS, IDEAL, GEMUK
+}
---
+++ app/src/main/java/org/d3if4027/hitungbmi/ui/AboutFragmenr.kt
@@ -0,0 +1,6 @@
+package org.d3if4027.hitungbmi.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if4027.hitungbmi.R
+
+class AboutFragment : Fragment(R.layout.fragment_about)
